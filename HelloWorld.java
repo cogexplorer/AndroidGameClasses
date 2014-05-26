829c266c95867a04eb3f1bd770d6f3f96bc8d4a0
Zhuang，Chao（庄超） <eczhuang@gmail.com>
2014-05-26 10:42:26 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.*;
 
-public class MainActivity extends Activity
+public class HelloWorld extends Activity
 {
     /** Called when the activity is first created. */
     @Override
